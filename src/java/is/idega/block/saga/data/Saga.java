1f1359899295c91a266279eadf1d9b3c49e13ae9
Martynas Stakė <martynas@idega.com>
2011-04-22 09:42:12 +0300

    Creating jpa+hibernate entity

---
+++ src/java/is/idega/block/saga/data/Saga.java
@@ -0,0 +1,12 @@
+package is.idega.block.saga.data;
+
+import java.io.Serializable;
+
+public class Saga implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7986373392792826654L;
+
+}
